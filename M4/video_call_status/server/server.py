1de21c80f8aa2dba4804a60d317e3bc72f71c939
pscaught <pscaught@gmail.com>
2023-05-13 02:51:14 -0700

    some cleanup for server.py

--- M4/video_call_status/server/server.py
+++ M4/video_call_status/server/server.py
@@ -6,7 +6,7 @@ import threading
 import subprocess
 from flask import Flask, jsonify
 
-DEBUG = False
+DEBUG = True
 
 app = Flask(__name__)
 
@@ -46,65 +46,60 @@ def update_data():
     with subprocess.Popen(
         cmd, stdout=subprocess.PIPE, stderr=subprocess.STDOUT
     ) as stream:
-        builtin_camera = False
-        previous_microphone_state = ""
-        previous_camera_state = ""
-        usb_camera = False
-        microphone = False
+        previous_cam_active_state = True
+        previous_mic_active_state = True
+        cam_active_state = False
+        mic_active_state = False
 
-        data = {"camActive": False, "micActive": False}
+        states = {"camActive": cam_active_state, "micActive": mic_active_state}
 
-        for _l in stream.stdout:
-            line = _l.decode("utf-8").rstrip()
+        for line in stream.stdout:
+            line = line.decode("utf-8").rstrip()
 
             # Microphone
             if "numRunningAudioEngines =" in line:
                 try:
-                    microphone = bool(int(line[-1]) > 0)
-                    # if int(line[-1]) > 0:
-                    #    microphone = True
-                    # else:
-                    #    microphone = False
+                    mic_active_state = bool(int(line[-1]) > 0)
                 except ValueError:
                     continue
-                if microphone:
-                    microphone_state = "active"
-                    data["micActive"] = True
-                else:
-                    microphone_state = "not active"
-                    data["micActive"] = False
 
-                if microphone_state is not previous_microphone_state:
-                    if DEBUG:
-                        print(f"Microphone is {microphone_state}")
-                    write_file(data)
-                    previous_microphone_state = microphone_state
+                states["micActive"] = mic_active_state
+
+            if mic_active_state != previous_mic_active_state:
+                if DEBUG:
+                    print(
+                        f"Microphone is {'active' if mic_active_state else 'not active'}"
+                    )
+                write_file(states)
+                previous_mic_active_state = mic_active_state
 
             # Camera
-            if "kCameraStreamStart" in line:
-                builtin_camera = True
-            if '"VDCAssistant_Power_State" = On;' in line:
-                usb_camera = True
-            if "kCameraStreamStop" in line:
-                builtin_camera = False
-            if '"VDCAssistant_Power_State" = Off;' in line:
-                usb_camera = False
+            if (
+                "kCameraStreamStart" in line
+                or '"VDCAssistant_Power_State" = On;' in line
+            ):
+                cam_active_state = True
+            elif (
+                "kCameraStreamStop" in line
+                or '"VDCAssistant_Power_State" = Off;' in line
+            ):
+                cam_active_state = False
 
-            if usb_camera or builtin_camera:
-                camera_state = "active"
-                data["camActive"] = True
-            else:
-                camera_state = "not active"
-                data["camActive"] = False
-            if camera_state is not previous_camera_state:
+            states["camActive"] = cam_active_state
+
+            if cam_active_state != previous_cam_active_state:
                 if DEBUG:
-                    print(f"Camera is {camera_state}")
-                write_file(data)
-                previous_camera_state = camera_state
+                    print(f"Camera is {'active' if cam_active_state else 'not active'}")
+                write_file(states)
+                previous_cam_active_state = cam_active_state
 
-
-update_thread = threading.Thread(target=update_data)
-update_thread.start()
 
 if __name__ == "__main__":
-    app.run(host="192.168.10.10", port=8000, debug=DEBUG)
+    flask_thread = threading.Thread(
+        target=lambda: app.run(
+            host="192.168.10.10", port=8000, debug=True, use_reloader=False
+        ),
+        daemon=True,
+    )
+    flask_thread.start()
+    update_data()
